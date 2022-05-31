f1cbd1aa7feed92a83d2c5f066e192f50afd2330
Mikhail Amelin <tsp.amelin@gmail.com>
2022-05-31 13:33:37 +0500

    add the business entities

---
+++ ta-application/src/main/java/com/amelin/traveladviser/domain/entity/person/Account.java
@@ -0,0 +1,10 @@
+package com.amelin.traveladviser.domain.entity.person;
+
+import com.amelin.traveladviser.domain.entity.base.AbstractEntity;
+
+/**
+ * Entity that encapsulates user of the application
+ * @author Mike Amelin
+ */
+public class Account extends AbstractEntity {
+}
---
+++ ta-application/src/main/java/com/amelin/traveladviser/domain/entity/transport/TransportType.java
@@ -0,0 +1,10 @@
+package com.amelin.traveladviser.domain.entity.transport;
+
+/**
+ * Type of the station/transport/vehicle
+ * that we support in the application
+ * @author Mike Amelin
+ */
+public enum TransportType {
+    AUTO, RAILWAY, AVIA
+}
