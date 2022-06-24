233644bc8ec17490ac8b1b584eff78a5286798a4
Mikhail Amelin <tsp.amelin@gmail.com>
2022-06-24 16:33:02 +0500

    added saveCity, findCityById methods and changed implementation of the findCities method in CityResource. v0.1.5

--- ta-presentation/src/main/java/com/amelin/traveladviser/app/rest/service/CityResource.java
+++ ta-presentation/src/main/java/com/amelin/traveladviser/app/rest/service/CityResource.java
@@ -1,23 +1,81 @@
 package com.amelin.traveladviser.app.rest.service;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-import jersey.repackaged.com.google.common.collect.Lists;
+import com.amelin.traveladviser.app.rest.service.base.BaseResource;
+import com.amelin.traveladviser.application.dto.CityDTO;
+import com.amelin.traveladviser.application.port.GeographicService;
+import com.amelin.traveladviser.application.transform.Transformer;
+import com.amelin.traveladviser.application.transform.impl.SimpleDTOTransformer;
+import com.amelin.traveladviser.application.usecase.GeographicServiceImpl;
+import com.amelin.traveladviser.domain.entity.geography.City;
+import com.amelin.traveladviser.domain.entity.transport.TransportType;
+import org.apache.commons.lang3.math.NumberUtils;
 
 @Path("cities")
 /**
  * {@link CityResource} is REST web-service that handles city-related requests
  * @author Mike Amelin
  */
-public class CityResource {
+public class CityResource extends BaseResource {
+    /**
+     * Underlying source of data
+     */
+    private final GeographicService service;
+
+    /**
+     * DTO <-> Entity transformer
+     */
+    private final Transformer transformer;
+
+    public CityResource() {
+        this.transformer = new SimpleDTOTransformer();
+        this.service = new GeographicServiceImpl();
+
+        City city = new City("Odessa");
+        city.addStation(TransportType.AUTO);
+        service.saveCity(city);
+    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<String> findCities() {
-        return Lists.newArrayList("Odessa", "Kiyv");
+    /**
+     * Returns all the existing cities
+     * @return
+     */
+    public List<CityDTO> findCities() {
+        return service.findCities().stream().map(city -> transformer.transform(city, CityDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    /**
+     * Saves new city instance
+     */
+    public void saveCity(CityDTO cityDTO) {
+        service.saveCity(transformer.untransform(cityDTO, City.class));
+    }
+
+    @Path("/{cityId}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    /**
+     * Returns city with specified identifier
+     */
+    public Response findCityById(@PathParam("cityId") final String cityId) {
+        if (!NumberUtils.isNumber(cityId)) {
+            return BAD_REQUEST;
+        }
+        Optional<City> city = service.findCityById(NumberUtils.toInt(cityId));
+        if (!city.isPresent()) {
+            return NOT_FOUND;
+        }
+        return ok(transformer.transform(city.get(), CityDTO.class));
     }
 }
--- ta-presentation/src/test/java/com/amelin/traveladvidser/app/rest/service/CityResourceTest.java
+++ ta-presentation/src/test/java/com/amelin/traveladvidser/app/rest/service/CityResourceTest.java
@@ -1,14 +1,18 @@
 package com.amelin.traveladvidser.app.rest.service;
 
 import com.amelin.traveladviser.app.rest.service.config.JerseyConfig;
+import com.amelin.traveladviser.application.dto.CityDTO;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Map;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * {@link CityResourceTest} is integration test that verifies
@@ -24,9 +28,42 @@ public class CityResourceTest extends JerseyTest {
 
     @Test
     public void testFindCitiesSuccess() {
-        List<?> cities = target("cities").request().get(List.class);
+        List<Map<String, String>> cities = target("cities").request().get(List.class);
         assertNotNull(cities);
-        assertTrue(cities.contains("Odessa"));
-        assertTrue(cities.contains("Kiyv"));
+        assertEquals(cities.size(), 1);
+
+        Map<String, String> city = cities.get(0);
+        assertEquals(city.get("name"), "Odessa");
+    }
+
+    @Test
+    public void testFindCityByIdSuccess() {
+        CityDTO city = target("cities/1").request().get(CityDTO.class);
+        assertNotNull(city);
+        assertEquals(city.getId(), 1);
+        assertEquals(city.getName(), "Odessa");
+    }
+
+    @Test
+    public void testFindCityByIdNotFound() {
+        Response response = target("cities/2").request().get(Response.class);
+        assertNotNull(response);
+        assertEquals(response.getStatus(), Response.Status.NOT_FOUND.getStatusCode());
+    }
+
+    @Test
+    public void testFindCityByIdInvalidId() {
+        Response response = target("cities/aaab").request().get(Response.class);
+        assertNotNull(response);
+        assertEquals(response.getStatus(), Response.Status.BAD_REQUEST.getStatusCode());
+    }
+
+    @Test
+    public void testSaveCitySuccess() {
+        CityDTO city = new CityDTO();
+        city.setName("Kiev");
+
+        Response response = target("cities").request().post(Entity.entity(city, MediaType.APPLICATION_JSON));
+        assertEquals(response.getStatus(), Response.Status.NO_CONTENT.getStatusCode());
     }
 }
